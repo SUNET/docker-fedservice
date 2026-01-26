b901305f2f7336d11f7d7ea8263719d2dc2b488b
Björn Mattsson <bjorn@sunet.se>
2026-01-26 13:39:49 +0100

    Updated to fedservice from Roland

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ if [ -d "${GIT_DIR}" ]; then
     rm -rf "${GIT_DIR}"
 fi
 echo "Using ${UPSTREAM_VERSION}."
-git clone --branch "${UPSTREAM_VERSION=master}" --depth 1 https://github.com/mikaelfrykholm/fedservice.git "${GIT_DIR}"
+git clone --branch "${UPSTREAM_VERSION=master}" --depth 1 https://github.com/SUNET/fedservice.git "${GIT_DIR}"
 pushd "${GIT_DIR}"
 pushd docker
 ./build.sh
